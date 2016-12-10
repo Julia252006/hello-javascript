030d2036227f5830641b192f99eebeb167e19864
Julia252006 <Julia252006@ukr.net>
2016-12-10 16:30:32 +0200

    README.md file added  link to published (fixing #3) commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,3 @@
 document.write('<h1>Hello, JavaScript!</h1>');
-document.write('<h1>it is a good day today!</h1>');
-document.write('<h1>I\'m glad to see you here! :)!</h1>');
+document.write('<h1>It is a good day today!</h1>');
+
